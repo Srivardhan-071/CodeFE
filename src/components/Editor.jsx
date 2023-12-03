2d13c3c35302d573d2f4ad401e682487a77e3c00
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2023-12-03 23:32:32 +0530

    resloved issue with backspace in editors

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -21,18 +21,26 @@ export default function Editor(props) {
         <>
             <h4 className="mode-name">{displayName}</h4>
             <AceEditor
+
                 className="ace-editor"
                 height={height}
                 width={width}
+                setShowInvisibles={true}
                 setOptions={{
-                    useWorker: false
+                    showInvisibles: false,
+                    useWorker: false,
+                    fontFamily: "monospace",
+                    useSoftTabs: false,
+                    tabSize: 4,
                 }}
                 mode={language}
+                enableLiveAutocompletion={true}
+                enableBasicAutocompletion={true}
+                focus={true}
                 theme="dracula"
                 onChange={onChange}
                 name="UNIQUE_ID_OF_DIV"
                 defaultValue={codes ? "<div></div>\n<style>\n\tdiv {\n\t\twidth: 100px;\n\t\theight: 100px;\n\t\tbackground: #17181a;\n\t}\n</style>" : ""}
-                fontSize="15px"
                 editorProps={{ $blockScrolling: true }}
             />
         </>
