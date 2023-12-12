d5bfd0926663e43384796718957f2e426ec64080
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2023-12-13 00:38:42 +0530

    added smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useEffect } from 'react';
 import Body from './components/Body';
 import ChallangeEditor from './components/ChallangeEditor';
 import Explore from './components/Explore';
@@ -6,8 +7,16 @@ import IDE from './components/IDE';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { Routes, Route } from 'react-router-dom';
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
+  useEffect(() => {
+    /* eslint-disable */
+    const scroll = new LocomotiveScroll({
+      smooth: true
+    })
+  }, [])
+
   return (
     <>
       <Navbar />
