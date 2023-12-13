6086a8b7fae36629283fd5308119ca1e3eec3058
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2023-12-14 00:13:42 +0530

    added ball chasing cursor animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Body from './components/Body';
 import ChallangeEditor from './components/ChallangeEditor';
 import Explore from './components/Explore';
@@ -8,19 +8,48 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { Routes, Route } from 'react-router-dom';
 import LocomotiveScroll from 'locomotive-scroll';
+import { motion } from "framer-motion"
 
 function App() {
+  const [mousePoistion, setMousePoistion] = useState({
+    x: 0,
+    y: 0
+  })
+
+  console.log(mousePoistion.x, mousePoistion.y)
+
   useEffect(() => {
     /* eslint-disable */
     const scroll = new LocomotiveScroll({
       smooth: true
     })
+    const mouseMove = e => {
+      setMousePoistion({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+    window.addEventListener("mousemove", mouseMove)
+    return () => {
+      window.removeEventListener("mousemove", mouseMove)
+    }
   }, [])
+
+  const variants = {
+    default: {
+      x: mousePoistion.x - 5,
+      y: mousePoistion.y - 5
+    }
+  }
 
   return (
     <>
+      <motion.div
+        className="mouse"
+        variants={variants}
+        animate="default"
+      />
       <Navbar />
-
       <div className="main">
         <Sidebar />
         <Routes>
